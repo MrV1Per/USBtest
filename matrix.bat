dd5ac1d5430a4bdf9648818e68a3363279a81d99
Vladyslav <61952283+MrV1Per@users.noreply.github.com>
2024-01-13 20:28:09 +0200

    Add files via upload

---
+++ matrix.bat
@@ -0,0 +1,8 @@
+@echo off
+color 0A
+title Matrix Effect
+
+:matrix
+echo %random% %random% %random% %random% %random% %random% %random% %random% %random% %random%
+timeout /nobreak /t 1 >nul
+goto matrix
